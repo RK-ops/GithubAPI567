be03023333719a0b2fc1bbd2d6d6f52efbbec044
RK-ops <63204698+RK-ops@users.noreply.github.com>
2022-10-07 17:31:29 -0400

    Update get_repo.py

--- get_repo.py
+++ get_repo.py
@@ -10,8 +10,8 @@ import json
 
 def connect(user):
     code200 = True
-    repo_url = "https://api.github.com/users/RK-ops/repos" ## % (userid)
-    commit_url = "https://api.github.com/repos/RK-ops/GithubAPI567/commits" ## % (id, repo)
+    repo_url = "https://api.github.com/users/%s/repos" ## % (userid)
+    commit_url = "https://api.github.com/repos/%s/%s/commits" ## % (id, repo)
 
     ## Get User
     repo_page = requests.get(repo_url % user)
