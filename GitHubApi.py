9159b98673ab457d54fa65118a2098ac045cd358
RK-ops <63204698+RK-ops@users.noreply.github.com>
2022-10-26 20:13:10 -0400

    Update and rename Test_Gitmock.py to GitHubApi.py

---
+++ GitHubApi.py
@@ -0,0 +1,18 @@
+# By Shashank Ramesh Kumar
+import requests
+import json
+
+def getrepo(username):
+    object = requests.get("https://api.github.com/users/" + username + "/repos")
+    if object.status_code != 200:
+        print("User Invalid")
+        return False
+    object = object.json()
+    if len(object) == 0:
+        print("There are no repository")
+        return False
+    for repo in object:
+        repoObj = requests.get(repo['commits_url'].split("{")[0])
+        repoObj = repoObj.json()
+        print("Repo: " + repo['name'] + "      Number Of Commits: " + str(len(repoObj)))
+    return True
